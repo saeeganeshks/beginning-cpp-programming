11ea8dcacc15049a8bafa5114d506d5720ec476b
Saee Ganesh K S <saeeganeshks@gmail.com>
2021-04-23 15:50:13 +0530

    Section 5 Exercise 3 Solution

---
+++ Section 5/Exercise 3/exercise.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void date_of_birth() {
+    int m {};
+    int d {};
+    int y {};
+    
+    cin >> m >> d >> y;
+    
+    cout << m << " " << d << " " << y;
+}
